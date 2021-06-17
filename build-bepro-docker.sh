bcc9fb83656d6da880a9717d1653fb943a1dc33b
sebatyler <sebatyler@gmail.com>
2021-06-17 12:16:46 +0900

    Improve

--- build-bepro-docker.sh
+++ build-bepro-docker.sh
@@ -3,30 +3,31 @@
 # Parse command line arguments.
 prune=false
 push=false
-for key in "$@"; do
+for key in "$@"
+do
     case $key in
-        --prune)
-        prune=true
-        ;;
-        --push)
-        push=true
-        ;;
+        --prune) prune=true ;;
+        --push) push=true ;;
     esac
 done
 
 # Read the project version.
 PROJECT_VERSION="$(cat ./tensorflow_cc/PROJECT_VERSION)"
 
-for tag in ubuntu-cuda; do
-    docker build --pull -t bepro/tensorflow_cc:${tag} -f Dockerfiles/${tag} .
-    docker tag bepro/tensorflow_cc:${tag} bepro/tensorflow_cc:${tag}-"${PROJECT_VERSION}"
-    if $push; then
-        docker push bepro/tensorflow_cc:${tag}
-        docker push bepro/tensorflow_cc:${tag}-"${PROJECT_VERSION}"
-    fi
-    if $prune; then
-        docker rmi bepro/tensorflow_cc:${tag}
-        docker rmi bepro/tensorflow_cc:${tag}-"${PROJECT_VERSION}"
-        docker system prune -af
-    fi
-done
+tag=ubuntu-cuda
+
+docker build --pull -t bepro/tensorflow_cc:${tag} -f Dockerfiles/${tag} .
+docker tag bepro/tensorflow_cc:${tag} bepro/tensorflow_cc:${tag}-"${PROJECT_VERSION}"
+
+if $push
+then
+    docker push bepro/tensorflow_cc:${tag}
+    docker push bepro/tensorflow_cc:${tag}-"${PROJECT_VERSION}"
+fi
+
+if $prune
+then
+    docker rmi bepro/tensorflow_cc:${tag}
+    docker rmi bepro/tensorflow_cc:${tag}-"${PROJECT_VERSION}"
+    docker system prune -af
+fi
